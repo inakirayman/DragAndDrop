e2b6a999b9158e7bb43ca4140be6e376c780cc8c
inakirayman <inaki337@gmail.com>
2022-10-06 23:48:59 +0200

    Card Script
    drag and drop logic

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IDragHandler,IBeginDragHandler,IEndDragHandler
+{
+
+    private GameObject _copy;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _copy = Instantiate(transform.gameObject,transform.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _copy.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if(Physics.Raycast(ray, out hit , 100) && hit.collider.tag == "CardStack")
+        {
+            Destroy(_copy);
+            gameObject.SetActive(false);
+        }
+        else
+        Destroy(_copy);
+
+    }
+}
